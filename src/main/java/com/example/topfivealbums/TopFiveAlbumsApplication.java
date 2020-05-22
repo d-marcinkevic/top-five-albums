df68cbd0596026c06415738889291cf1a34d4af8
d-marcinkevic <d.marcinkevic@adform.com>
2020-05-22 21:12:01 +0300

    first commit

---
+++ src/main/java/com/example/topfivealbums/TopFiveAlbumsApplication.java
@@ -0,0 +1,13 @@
+package com.example.topfivealbums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopFiveAlbumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopFiveAlbumsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/topfivealbums/TopFiveAlbumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.topfivealbums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopFiveAlbumsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
